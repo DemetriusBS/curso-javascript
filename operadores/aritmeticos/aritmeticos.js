d3af9084bf5b322d52bb6a73e1d3ea94c371560e
Demetrius Batista dos Santos <dbsfox@outlook.com>
2020-11-16 15:17:31 -0300

    Aprendizado de operadores aritméticos: + e  -

---
+++ operadores/aritmeticos/aritmeticos.js
@@ -0,0 +1,23 @@
+//Adição
+var soma = 20 + 20;
+console.log('Soma de 20 + 20 é igual a: ', soma)
+console.log('Demetrius '+'Batista '+'Dos Santos')
+var numString = '27'
+console.log(numString)
+console.log(typeof numString)
+console.log(+numString)
+console.log(true + false + true)
+console.log('0.2 + 0.1 = ', 0.2 + 0.1)
+
+//Subtração
+
+console.log('#####Subtração#####')
+var sub = 500 - 330
+console.log('500 - 330 = ', sub)
+console.log('-120 - 30', -120 - 30)
+console.log('-120 + 30 = ', -120 + 30)
+console.log('15 - true = ', 15 - true)
+var numString = '56'
+console.log("-(numString = '56')",-numString)
+console.log("typeof -(numString = '56')", typeof -numString)
+console.log('0.33 - 0.1 = ', 0.33 - 0.1)
