e631af44521d177c54c9c349a4fc28bbf81d16a5
Demetrius Batista dos Santos <dbsfox@outlook.com>
2020-11-17 11:27:17 -0300

    Aprendizado de oparadores aritméticos

--- operadores/aritmeticos/aritmeticos.js
+++ operadores/aritmeticos/aritmeticos.js
@@ -1,7 +1,8 @@
 //Adição
+
 var soma = 20 + 20;
 console.log('Soma de 20 + 20 é igual a: ', soma)
-console.log('Demetrius '+'Batista '+'Dos Santos')
+console.log('Demetrius ' + 'Batista ' + 'Dos Santos')
 var numString = '27'
 console.log(numString)
 console.log(typeof numString)
@@ -18,6 +19,73 @@ console.log('-120 - 30', -120 - 30)
 console.log('-120 + 30 = ', -120 + 30)
 console.log('15 - true = ', 15 - true)
 var numString = '56'
-console.log("-(numString = '56')",-numString)
+console.log("-(numString = '56')", -numString)
 console.log("typeof -(numString = '56')", typeof -numString)
 console.log('0.33 - 0.1 = ', 0.33 - 0.1)
+
+//Multiplicação
+console.log('#####Multiplicação#####')
+var multi = 5 * 20
+console.log('5 * 20 = ', multi)
+console.log('7 * "3"= ', 7 * "3")
+console.log('7 * "tres"= ', 7 * "tres")
+console.log('Precedencia: ', 3 + 3 * 2)
+console.log('Precedencia: () ', (3 + 3) * 2)
+console.log("3 * 0 = ", 3 * 0)
+console.log("0 * 3 = ", 0 * 3)
+console.log("3 * -2 = ", 3 * -2)
+console.log('3 * true = ', 3 * true)
+
+
+//Divisão
+console.log('####Divisão######')
+var div = 10 / 2
+console.log('10 / 2 = ', div)
+console.log('10 / 0 = ', 10 / 0)
+console.log('0 / 10 = ', 0 / 10)
+console.log('-10 / 0 = ', -10 / 0)
+console.log(' 2 / 3 = ', 2 / 3)
+console.log('10 / "2" = ', 10 / "2")
+console.log('10 / "dois" = ', 10 / "dois")
+
+
+//Módulo-resto-divisão
+console.log('####Módulo######')
+var mod = 9 % 2
+console.log('9 % 2 = ', mod)
+
+
+//Incremento
+console.log('#####Incremento#####')
+var incremento = 10
+incremento++
+console.log('1 - Incremento 10 (++) = ', incremento)
+console.log('2 - Incremento++ = ', incremento++)
+console.log('3 - Incremento = ', incremento)
+console.log('4 - ++Incremento = ', ++incremento)
+console.log('5 - Incremento = ', incremento)
+
+//Decremento
+console.log('#####Decremento#####')
+var decremento = 20
+decremento--
+console.log('1 - decremento 20 (--) = ', decremento)
+console.log('2 - decremento-- = ', decremento--)
+console.log('3 - decremento = ', decremento)
+console.log('4 - --decremento = ', --decremento)
+console.log('5 - decremento = ', decremento)
+
+//**Exponenciação/Potenciação
+console.log('#####**Exponenciação/Potenciação#####')
+var exp = 2 ** 3
+console.log('2 elevado a 3 = ', exp)
+
+
+
+
+
+
+
+
+
+
