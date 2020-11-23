95346e96a722fa1b3f8504781f761877770e1bc2
Demetrius Batista dos Santos <dbsfox@outlook.com>
2020-11-23 14:57:40 -0300

    Estrutura de condição if ...else

---
+++ estrutura-de-controle/js-controle/if-else.js
@@ -0,0 +1,21 @@
+//if ... else
+var condicao = false
+
+if(condicao) console.log('0 - Entrou no bloco if')
+
+if(condicao){
+    console.log('1 - Entrou no bloco if')
+    console.log('2 - Entrou no bloco if')
+}
+
+if(condicao){
+    console.log('3 - Entrou no bloco if')
+}else{
+    console.log('4 - Entrou no bloco else')
+}
+
+if(condicao == true){
+    console.log('Bloco if')
+}else{
+    console.log('Bloco else')
+}
