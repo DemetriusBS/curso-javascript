4a1738aa55116c22c1e413e2153fcbc55f0c1caa
DemetriusBS <dbsfox@outlook.com>
2020-12-16 10:55:16 -0300

    Explorando o objeto Number - Parte 2

--- explorando-string-number-boolean/number.js
+++ explorando-string-number-boolean/number.js
@@ -24,3 +24,38 @@ console.log(v1, Number.isInteger(v1))
 console.log(v2, Number.isInteger(v2))
 console.log(v3, Number.isInteger(v3))
 console.log(v4, Number.isInteger(v4))
+
+//isNaN - Retorna true se for valor NaN
+var a = 'abc'
+var b = '32'
+var c = 23
+var d = NaN // Valor especial
+
+console.log(a, Number.isNaN(a))
+console.log(b, Number.isNaN(b))
+console.log(c, Number.isNaN(c))
+console.log(d, Number.isNaN(d))
+
+//toPrecision
+var numero = 2356.45698
+console.log(numero, numero.toPrecision(6), typeof numero.toPrecision(6))
+
+//valueOf - Retorna o valor primitivo do dado
+var valorX = new Number(23467)
+console.log(valorX.valueOf(), typeof valorX.valueOf())
+
+//isFinite
+console.log(2 / 2)
+console.log('isFinite: ', Number.isFinite(2 / 2))
+console.log(1000 / 1)
+console.log('isFinite: ', Number.isFinite(1000 / 1))
+console.log(1000 / 0)
+console.log('isFinite: ', Number.isFinite(1000 / 0))
+
+//Valores pré definidos no js
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.POSITIVE_INFINITY)
+console.log(Number.NEGATIVE_INFINITY)
+console.log(Number.NaN)
+console.log(valorX.toString().length)
