69426836c004b6e0a8ade67b1d58bb7bbd7c76de
DemetriusBS <dbsfox@outlook.com>
2021-02-11 10:15:51 -0300

    Array - Literal e Função Construtora

---
+++ array/array.js
@@ -0,0 +1,20 @@
+//Literal
+var array =[]
+array[0] = 'Teste'
+array.push('Teste 2')
+console.log('Exibir array: ', array)
+
+var nomes = ['Pedro', 'João', 'Maria']
+console.log('Nomes: ', nomes)
+console.log('nomes[0]: ', nomes[0])
+console.log('nomes.length: ', nomes.length)
+
+//Construtor
+var numeros = new Array(33, 47, 74, 88, 95)
+console.log('numeros: ', numeros)
+var numeros2 = new Array(2)
+console.log('numeros2: ', numeros2)
+console.log('numeros2.length: ', numeros2.length)
+numeros2[0] = 56
+numeros2[1] = 77
+console.log(numeros2)
